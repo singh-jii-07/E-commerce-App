1379899e6d051ff78d4167d7dd89bd9e7f60d0dd
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-07-16 14:08:37 +0530

    change password done

--- Server/auth/controllers/User.js
+++ Server/auth/controllers/User.js
@@ -17,7 +17,7 @@ const register = async (req, res) => {
       return res.status(400).json({
         message: "User already exists",
       });
-    } 
+    }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
