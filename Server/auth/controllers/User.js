0d72806c8008a0f25ba1f06d7f6a0baee410f011
singh-jii-07 <nileshkumarsingh060@gmail.com>
2026-07-16 12:37:14 +0530

    login and logout done

--- Server/auth/controllers/User.js
+++ Server/auth/controllers/User.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js"
+import User from "../models/User.js";
 const register = async (req, res) => {
   try {
     const { username, password, email } = req.body;
@@ -19,7 +19,6 @@ const register = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
 
     const newUser = await User.create({
       username,
@@ -41,4 +40,106 @@ const register = async (req, res) => {
   }
 };
 
-export { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+  
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and Password are required",
+      });
+    }
+
+   
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+   
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    user.token = token;
+    user.isLoggedIn = true;
+
+    await user.save();
+
+    const userData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      avatar: user.avatar,
+      role: user.role,
+      isLoggedIn: user.isLoggedIn,
+    };
+
+    return res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      token,
+      user: userData,
+    });
+  } catch (error) {
+    console.error("LOGIN ERROR:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+const Logout = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    await User.findByIdAndUpdate(userId, {
+      token: null,
+      isLoggedIn: false,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Logout Successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { register, login,Logout, };
--- Server/auth/routes/User.js
+++ Server/auth/routes/User.js
@@ -1,13 +1,11 @@
 import express from "express";
-import { register } from "../Controllers/User.js";
-
+import { register,login, Logout } from "../Controllers/User.js";
+import auth from "../Middleware/Auth.js";
 
 const userRoute = express.Router();
 
-userRoute.post(
-  "/register",
-  register
-);
+userRoute.post("/register", register);
+userRoute.post("/login", login);
+userRoute.post("/logout",auth,Logout)
 
 export default userRoute;
-
